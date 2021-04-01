71e30e5a32bb318cb6f6e472cdfd5d1493330290
Hamza Aburaneh <Hamza.Aburaneh@carleton.ca>
2021-04-01 09:01:27 -0400

    Update ContentService.java

--- src/main/java/ca/gc/tbs/service/ContentService.java
+++ src/main/java/ca/gc/tbs/service/ContentService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 public class ContentService {
 
 	public ContentService() {
-		//BadWords.loadConfigs();
+		BadWords.loadConfigs();
 	}
 
 	public String cleanContent(String content) {
