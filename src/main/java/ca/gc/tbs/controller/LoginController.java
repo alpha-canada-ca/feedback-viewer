713cc13c2cbc2d9def601c2138608d5c48dff8f0
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2023-02-13 11:17:52 -0500

    Implemented language-based dynamic view templates for the SUCCESS endpoint

--- src/main/java/ca/gc/tbs/controller/LoginController.java
+++ src/main/java/ca/gc/tbs/controller/LoginController.java
@@ -3,35 +3,30 @@ package ca.gc.tbs.controller;
 import ca.gc.tbs.domain.User;
 import ca.gc.tbs.service.EmailService;
 import ca.gc.tbs.service.UserService;
-
-import java.text.SimpleDateFormat;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.text.SimpleDateFormat;
+
 @Controller
 public class LoginController {
-	public static final String DATE_FORMAT = "yyyy-MM-dd";
-	public static final SimpleDateFormat format = new SimpleDateFormat(DATE_FORMAT);
+    public static final String DATE_FORMAT = "yyyy-MM-dd";
+    public static final SimpleDateFormat format = new SimpleDateFormat(DATE_FORMAT);
 
-	@Autowired
-	private UserService userService;
-	
-	@Autowired
-	private EmailService emailService;
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private EmailService emailService;
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public ModelAndView login(HttpServletRequest request) throws Exception {
@@ -41,58 +36,63 @@ public class LoginController {
         modelAndView.setViewName("login_" + lang);
         return modelAndView;
     }
-	
 
-	@RequestMapping(value = "/signup", method = RequestMethod.GET)
-	public ModelAndView signup() {
-		ModelAndView modelAndView = new ModelAndView();
-		User user = new User();
-		modelAndView.addObject("user", user);
-		modelAndView.addObject("institutions",this.userService.findInstitutions());
-		modelAndView.setViewName("signup");
-		return modelAndView;
-	}
 
-	@GetMapping("/checkExists")
-	public @ResponseBody String checkExists(@RequestParam String email) {
-		// verify that one has not already been created.
-		User userExists = userService.findUserByEmail(email);
-		if (userExists != null) {
-			return "true";
-		} else {
-			return "false";
-		}
-	}
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public ModelAndView signup() {
+        ModelAndView modelAndView = new ModelAndView();
+        User user = new User();
+        modelAndView.addObject("user", user);
+        modelAndView.addObject("institutions", this.userService.findInstitutions());
+        modelAndView.setViewName("signup");
+        return modelAndView;
+    }
 
-	@RequestMapping(value = "/signup", method = RequestMethod.POST)
-	public RedirectView createNewUser(@Valid User user, RedirectAttributes atts) {
-		userService.saveUser(user);
-		emailService.sendUserActivationRequestEmail(user.getEmail());
-		atts.addFlashAttribute("successMessage",
-				"User has been registered successfully. You will be notified when the account has been activated.");
-		return new RedirectView("success");
-	}
+    @GetMapping("/checkExists")
+    public @ResponseBody
+    String checkExists(@RequestParam String email) {
+        // verify that one has not already been created.
+        User userExists = userService.findUserByEmail(email);
+        if (userExists != null) {
+            return "true";
+        } else {
+            return "false";
+        }
+    }
 
-	@RequestMapping(value = { "/success" }, method = RequestMethod.GET)
-	public String success() {
-		return "success";
-	}
- 
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public RedirectView createNewUser(@Valid User user, RedirectAttributes atts) {
+        userService.saveUser(user);
+        emailService.sendUserActivationRequestEmail(user.getEmail());
+        atts.addFlashAttribute("successMessage",
+                "User has been registered successfully. You will be notified when the account has been activated.");
+        return new RedirectView("success");
+    }
+
+
+    @RequestMapping(value = {"/success"}, method = RequestMethod.GET)
+    public ModelAndView success(HttpServletRequest request) {
+        String lang = (String) request.getSession().getAttribute("lang");
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("success_" + lang);
+        return mav;
+    }
+
     //redirects localhost to sign in page.
-	@RequestMapping(value = { "/" }, method = RequestMethod.GET)
-	public View home(@Valid User user) {
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("home");
-		
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		RedirectView view = new RedirectView("/pageFeedback");
-		
-		//if user is not logged in, redirect to signin
-		if(auth.getName().equals("anonymousUser")) {
-			view = new RedirectView("signin");
-		}
-		
-		return view;
-	}
+    @RequestMapping(value = {"/"}, method = RequestMethod.GET)
+    public View home(@Valid User user) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home");
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        RedirectView view = new RedirectView("/pageFeedback");
+
+        //if user is not logged in, redirect to signin
+        if (auth.getName().equals("anonymousUser")) {
+            view = new RedirectView("signin");
+        }
+
+        return view;
+    }
 
 }
