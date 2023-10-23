45af026734ab92c0bfc6f26f0fd3fba1053ae75f
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2023-10-23 11:31:39 -0400

    Refactor BadWords class: Introduced thread-safe set, SLF4J logging, and optimize config loading.

--- src/main/java/ca/gc/tbs/service/BadWords.java
+++ src/main/java/ca/gc/tbs/service/BadWords.java
@@ -1,6 +1,8 @@
 package ca.gc.tbs.service;
 
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
@@ -9,114 +11,63 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class BadWords {
+    private static final Logger logger = LoggerFactory.getLogger(BadWords.class);
 
-    static Set<String> words = new HashSet<>();
+    private static final Set<String> words = Collections.newSetFromMap(new ConcurrentHashMap<>());
+    private static final String[] DEFAULT_FILES = {
+            "static/badwords/facebook_badwords_en.txt",
+            "static/badwords/youtube_badwords_en.txt",
+            "static/badwords/badwords_fr.txt",
+            "static/badwords/threats_fr.txt",
+            "static/badwords/threats_en.txt"
+    };
 
     public static void loadConfigs() {
-        loadGoogleConfigs();
-        loadFileConfigs("static/badwords/facebook_badwords_en.txt");
-        loadFileConfigs("static/badwords/youtube_badwords_en.txt");
-        loadFileConfigs("static/badwords/badwords_fr.txt");
-        loadFileConfigs("static/badwords/threats_fr.txt");
-        loadFileConfigs("static/badwords/threats_en.txt");
-        System.out.println("Loaded " + words.size() + " words to filter out");
+        for (String file : DEFAULT_FILES) {
+            loadFileConfigs(file);
+        }
+        loadGoogleConfigs("https://docs.google.com/spreadsheets/d/1hIEi2YG3ydav1E06Bzf2mQbGZ12kh2fe4ISgLg_UBuM/export?format=csv");
+        logger.info("Loaded {} words to filter out", words.size());
     }
 
-    public static void loadFileConfigs(String filePath) {
+    private static void loadFileConfigs(String filePath) {
         try {
             Resource resource = new ClassPathResource(filePath, BadWords.class.getClassLoader());
             String[] newWords = IOUtils.toString(resource.getInputStream(), StandardCharsets.UTF_8).split(",");
             for (String word : newWords) {
-                words.add(word.trim());
+                words.add(word.trim().toLowerCase());  // converting to lowercase for consistency
             }
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            logger.error("Error loading file config {}", filePath, e);
         }
     }
 
-    public static void loadGoogleConfigs() {
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(new URL(
-                    "https://docs.google.com/spreadsheets/d/1hIEi2YG3ydav1E06Bzf2mQbGZ12kh2fe4ISgLg_UBuM/export?format=csv")
-                    .openConnection().getInputStream()));
-            String line = "";
-
+    private static void loadGoogleConfigs(String googleSheetUrl) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new URL(googleSheetUrl).openConnection().getInputStream()))) {
+            String line;
             while ((line = reader.readLine()) != null) {
-                String[] content = null;
-                try {
-                    content = line.split(",");
-                    if (content.length == 0) {
-                        continue;
-                    }
-                    String word = content[0];
-                    words.add(word);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
+                words.add(line.trim().split(",")[0].toLowerCase()); // assuming first column has the word
             }
-
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error("Error loading Google config from {}", googleSheetUrl, e);
         }
-
     }
 
-    static String censor(String text) {
-
-        // text = removeLeetSpeak(text);
-        // Break down sentence by ' ' spaces
-        // and store each individual word in
-        // a different list
-        String[] word_list = text.split("\\s+");
-
-        // A new string to store the result
+    public static String censor(String text) {
         StringBuilder result = new StringBuilder();
-
-        // Iterating through our list
-        // of extracted words
-        int index = 0;
-        for (String i : word_list) {
-            String wordToCheck = i;
-            wordToCheck = wordToCheck.toLowerCase().replaceAll("[^a-zA-Z]", "");
-            if (words.contains(wordToCheck)) {
-                // changing the censored word to
-                // created asterisks censor
-                word_list[index] = createMask(i);
-            }
-            index++;
+        for (String word : text.split("\\s+")) {
+            String wordToCheck = word.toLowerCase().replaceAll("[^a-zà-ÿ]", ""); // Including accented characters for French
+            result.append(words.contains(wordToCheck) ? createMask(word) : word).append(' ');
         }
-
-        // join the words
-        for (String i : word_list)
-            result.append(i).append(' ');
-
         return result.toString().trim();
     }
 
-    public static String createMask(String word) {
-        StringBuilder mask = new StringBuilder();
-        for (int i = 0; i < word.length(); i++) {
-            mask.append("#");
-        }
-        return mask.toString();
+    private static String createMask(String word) {
+        return word.replaceAll(".", "#");
     }
-
-    public static class LengthComparator implements java.util.Comparator<String> {
-
-        public int compare(String s1, String s2) {
-            if (s1.length() > s2.length()) {
-                return -1;
-            } else if (s1.length() < s2.length()) {
-                return 1;
-            } else {
-                return s1.compareTo(s2);
-            }
-        }
-    }
-
 }
