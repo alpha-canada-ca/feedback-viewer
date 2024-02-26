e15742c4a3e66e7b03b5fca1bb18ec505ad9ce3f
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2024-02-26 11:02:47 -0500

    Use custom download buttons

--- src/main/resources/static/js/pageFeedback.js
+++ src/main/resources/static/js/pageFeedback.js
@@ -178,56 +178,28 @@ $(document).ready(function () {
   function newexportaction(e, dt, button, config) {
     var self = this;
     var oldStart = dt.settings()[0]._iDisplayStart;
-    dt.one("preXhr", function (e, s, data) {
-      // Just this once, load all data from the server...
-      data.start = 0;
-      data.length = 2147483647;
-      dt.one("preDraw", function (e, settings) {
-        if (button[0].className.indexOf("buttons-excel") >= 0) {
-          $.fn.dataTable.ext.buttons.excelHtml5.available(dt, config)
-            ? $.fn.dataTable.ext.buttons.excelHtml5.action.call(
-                self,
-                e,
-                dt,
-                button,
-                config
-              )
-            : $.fn.dataTable.ext.buttons.excelFlash.action.call(
-                self,
-                e,
-                dt,
-                button,
-                config
-              );
-        } else if (button[0].className.indexOf("buttons-csv") >= 0) {
-          $.fn.dataTable.ext.buttons.csvHtml5.available(dt, config)
-            ? $.fn.dataTable.ext.buttons.csvHtml5.action.call(
-                self,
-                e,
-                dt,
-                button,
-                config
-              )
-            : $.fn.dataTable.ext.buttons.csvFlash.action.call(
-                self,
-                e,
-                dt,
-                button,
-                config
-              );
-        }
-        dt.one("preXhr", function (e, s, data) {
-          // DataTables thinks the first item displayed is index 0, but we're not drawing that.
-          // Set the property to what it was before exporting.
-          settings._iDisplayStart = oldStart;
-          data.start = oldStart;
-        }); // Reload the grid with the original page. Otherwise, API functions like table.cell(this) don't work properly.
-        setTimeout(dt.ajax.reload, 0); // Prevent rendering of the full data to the DOM
-        return false;
-      });
+    dt.one('preXhr', function (e, s, data) {
+        // Just this once, load all data from the server...
+        data.start = 0;
+        data.length = 2147483647;
+        dt.one('preDraw', function (e, settings) {
+            if (button[0].className.indexOf('buttons-excel') >= 0) {
+                $.fn.dataTable.ext.buttons.excelHtml5.available(dt, config) ? $.fn.dataTable.ext.buttons.excelHtml5.action.call(self, e, dt, button, config) : $.fn.dataTable.ext.buttons.excelFlash.action.call(self, e, dt, button, config);
+            } else if (button[0].className.indexOf('buttons-csv') >= 0) {
+                $.fn.dataTable.ext.buttons.csvHtml5.available(dt, config) ? $.fn.dataTable.ext.buttons.csvHtml5.action.call(self, e, dt, button, config) : $.fn.dataTable.ext.buttons.csvFlash.action.call(self, e, dt, button, config);
+            }
+            dt.one('preXhr', function (e, s, data) {
+                // DataTables thinks the first item displayed is index 0, but we're not drawing that.
+                // Set the property to what it was before exporting.
+                settings._iDisplayStart = oldStart;
+                data.start = oldStart;
+            }); // Reload the grid with the original page. Otherwise, API functions like table.cell(this) don't work properly.
+            setTimeout(dt.ajax.reload, 0); // Prevent rendering of the full data to the DOM
+            return false;
+        });
     }); // Requery the server with the new one-time export settings
     dt.ajax.reload();
-  }
+};
 
   $(".reset-filters").on("click", function () {
     resetFilters();
@@ -281,6 +253,15 @@ $(document).ready(function () {
     // Reload DataTables to reflect the reset date range
     table.ajax.reload();
   });
+  $('#downloadCSV').on('click', function(e) { // Notice the 'e' parameter here
+    e.preventDefault(); // Use the passed event object
+    table.button('.buttons-csv').trigger();
+});
+
+$('#downloadExcel').on('click', function(e) { // Notice the 'e' parameter here
+    e.preventDefault(); // Use the passed event object
+    table.button('.buttons-excel').trigger();
+});
 
   $("#language, #department, #section, #theme").on("change", function () {
     table.ajax.reload();
