86b30fd3b9d40024d98f28c28d63ad6e126078dd
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2024-04-03 13:19:34 -0400

    Refactored cache management and logging in ProblemCacheService

--- src/main/java/ca/gc/tbs/service/ProblemCacheService.java
+++ src/main/java/ca/gc/tbs/service/ProblemCacheService.java
@@ -2,46 +2,35 @@ package ca.gc.tbs.service;
 
 import ca.gc.tbs.domain.Problem;
 import ca.gc.tbs.repository.ProblemRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.context.ApplicationListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
-public class ProblemCacheService implements ApplicationListener<ApplicationReadyEvent> {
+public class ProblemCacheService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProblemCacheService.class);
     @Autowired
     private ProblemRepository problemRepository;
 
-    // This method will be triggered once the application is fully ready
-    @Override
-    public void onApplicationEvent(ApplicationReadyEvent event) {
-        getDistinctUrls(); // Populate the cache
-    }
-
-    @Scheduled(cron = "0 0 0 * * *") // Runs every day at midnight
+    @Scheduled(cron = "0 0 0 * * *")
     @CacheEvict(value = "distinctUrls", allEntries = true)
     public void clearCacheDaily() {
         // This method will automatically clear the cached distinct URLs daily
         System.out.println("EVICTING CACHE");
-
-    }
-
-    // Method to refresh cache, can be called based on a scheduler or an event
-    @CacheEvict(value = "distinctUrls", allEntries = true)
-    public void invalidateDistinctUrlsCache() {
-        System.out.println("EVICTING CACHE");
-        // This method will force the next call to refreshDistinctUrls() to fetch fresh data
     }
 
     @Cacheable("distinctUrls")
     public List<Problem> getDistinctUrls() {
         System.out.println("Fetching distinct URLs from the database");
-        List<Problem> x = problemRepository.findAllTest();
-        System.out.println("done");
-        return x;
+        List<Problem> result = problemRepository.findAllTest();
+        System.out.println("Fetched " + result.size() + " distinct URLs");
+        return result;
     }
 }
