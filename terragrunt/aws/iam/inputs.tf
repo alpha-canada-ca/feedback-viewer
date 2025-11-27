cfbbfc41adad3513f6c093b9edf049aadff131da
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2025-11-27 14:01:37 -0500

    refactor: clean up IAM module and update staging config

--- terragrunt/aws/iam/inputs.tf
+++ terragrunt/aws/iam/inputs.tf
@@ -1,23 +1,3 @@
-variable "product_name" {
-  description = "(Required) The name of the product you are deploying."
-  type        = string
-}
-
-variable "env" {
-  description = "The current running environment"
-  type        = string
-}
-
-variable "billing_code" {
-  description = "The billing code to tag our resources with"
-  type        = string
-}
-
-variable "default_tags" {
-  description = "The default tags we apply to all resources"
-  type        = map(string)
-}
-
 variable "docdb_username_arn" {
   description = "The ARN of the DocumentDB username SSM parameter"
   type        = string
