c8e8694f27c03a74a82891e78b9baf66e8a98034
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2025-11-27 14:01:11 -0500

    refactor: remove DocumentDB credentials from SSM module

--- terragrunt/aws/ssm/outputs.tf
+++ terragrunt/aws/ssm/outputs.tf
@@ -1,13 +1,3 @@
-output "docdb_username_arn" {
-  description = "The SSM parameter ARN for the DocumentDB username"
-  value       = aws_ssm_parameter.docdb_username.arn
-}
-
-output "docdb_password_arn" {
-  description = "The SSM parameter ARN for the DocumentDB password"
-  value       = aws_ssm_parameter.docdb_password.arn
-}
-
 output "jwt_secret_key_arn" {
   description = "The SSM parameter ARN for the JWT secret key"
   value       = aws_ssm_parameter.jwt_secret_key.arn
