d56a5214d35c102ef663854c95e8aab47739d9f7
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2025-11-28 10:42:09 -0500

    fix: update ssm value to not conflict

--- terragrunt/aws/ssm/ssm.tf
+++ terragrunt/aws/ssm/ssm.tf
@@ -2,7 +2,7 @@
 # Reference them via env_vars.hcl: dto_feedback_cj_docdb_username_arn and dto_feedback_cj_docdb_password_arn
 
 resource "aws_ssm_parameter" "jwt_secret_key" {
-  name  = "jwt_secret_key"
+  name  = "/feedback-viewer/staging/jwt_secret_key"
   type  = "SecureString"
   value = var.jwt_secret_key
 
