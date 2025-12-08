339bbaaf0781c53447bb350573cde3151e526ec8
Hamza Aburaneh <Hamza.aburaneh@carleton.ca>
2025-12-08 11:58:24 -0500

    chore: add GC IP cache and filter configuration

--- src/main/java/ca/gc/tbs/config/CacheConfig.java
+++ src/main/java/ca/gc/tbs/config/CacheConfig.java
@@ -14,6 +14,6 @@ public class CacheConfig {
 
   @Bean
   public CacheManager cacheManager() {
-    return new ConcurrentMapCacheManager("problemDates", "distinctUrls", "processedProblems");
+    return new ConcurrentMapCacheManager("problemDates", "distinctUrls", "processedProblems", "gcIpCache");
   }
 }
